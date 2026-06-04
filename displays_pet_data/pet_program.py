161db23e103ba4c392bf6228869064c81b3f3cc5
unknown <jhullianchica@gmail.com>
2026-06-04 18:24:32 +0800

    Added input for pet attributes

--- displays_pet_data/pet_program.py
+++ displays_pet_data/pet_program.py
@@ -1,3 +1,23 @@
 from pet import Pet
 
 pet = Pet()
+
+print("PET REGISTRATION")
+
+name = input("Enter pet name: ").strip()
+pet.set_name(name)
+
+animal_type = input("Enter animal type: ").strip()
+pet.animal_type(animal_type)
+
+while True:
+    try:
+        age = int(input("Enter pet age: "))
+        if age < 0:
+            print("Age cannot be zero or less than zero. Try again")
+            continue
+        pet.set_age(age)
+        break
+    except ValueError:
+        print("Please enter a valid integer.")
+    
