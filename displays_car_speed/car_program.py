d81cd8e3a923134e239229ea074a8b824c29685b
unknown <jhullianchica@gmail.com>
2026-06-04 17:17:13 +0800

    Added displays for speed while accelerating and braking

--- displays_car_speed/car_program.py
+++ displays_car_speed/car_program.py
@@ -10,5 +10,16 @@ print("2...")
 time.sleep(0.5)
 print("1...")
 time.sleep(0.5)
-print("GO!")
+print("GO!\n")
 
+print("ACCELERATING!")
+for i in range(5):
+    car.accelerate()
+    print(f"Accelerating... Current speed: {car.get_speed()} km/h")
+
+print("\nBRAKING!")
+for i in range(5):
+    car.brake()
+    print(f"Braking... Current speed: {car.get_speed()} km/h")
+
+print("\nCar stopped.")
