adad0ec6e93ef679c0d85c66ced1db88651ba438
unknown <jhullianchica@gmail.com>
2026-06-04 17:09:43 +0800

    Added timer

--- displays_car_speed/car_program.py
+++ displays_car_speed/car_program.py
@@ -1,3 +1,14 @@
 from car import Car
+import time
 
 car = Car(1993, "Toyota Supra")
+
+print("Ready...")
+print("3...")
+time.sleep(0.5)
+print("2...")
+time.sleep(0.5)
+print("1...")
+time.sleep(0.5)
+print("GO!")
+
