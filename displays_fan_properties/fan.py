ed7c21b32cf1d3eaaaeadfc70517338ce8d7d4bd
unknown <jhullianchica@gmail.com>
2026-06-04 15:53:53 +0800

    Fixed getter and setter methods indentation

--- displays_fan_properties/fan.py
+++ displays_fan_properties/fan.py
@@ -9,26 +9,26 @@ class Fan:
         self.__color = color
         self.__on = on
 
-        def get_speed(self):
-            return self.__speed
+    def get_speed(self):
+        return self.__speed
 
-        def set_speed(self, speed):
-            self.__speed = speed
+    def set_speed(self, speed):
+        self.__speed = speed
 
-        def get_radius(self):
-            return self.__radius
+    def get_radius(self):
+        return self.__radius
 
-        def set_radius(self, radius):
-            self.__radius = radius
+    def set_radius(self, radius):
+        self.__radius = radius
 
-        def get_color(self):
-            return self.__color
+    def get_color(self):
+        return self.__color
 
-        def set_color(self, color):
-            self.__color = color
+    def set_color(self, color):
+        self.__color = color
 
-        def get_on(self):
-            return self.__on
+    def get_on(self):
+        return self.__on
 
-        def set_on(self, on):
-            self.__on = on
+    def set_on(self, on):
+        self.__on = on
