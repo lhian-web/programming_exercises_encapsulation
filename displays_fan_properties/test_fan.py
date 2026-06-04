360077276d65cc14128c198b9c6cb0fcd0d4f588
unknown <jhullianchica@gmail.com>
2026-06-04 16:12:04 +0800

    Added print to display fan properties

--- displays_fan_properties/test_fan.py
+++ displays_fan_properties/test_fan.py
@@ -3,3 +3,16 @@ from fan import Fan
 fan1 = Fan(Fan.FAST, 10, "Yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "Blue", False)
 
+print("\nFAN PROPERTIES")
+
+print("\nFAN1:")
+print(f"Speed : {fan1.get_speed()}")
+print(f"Radius : {fan1.get_radius()}")
+print(f"Color : {fan1.get_color()}")
+print(f"Status : {"ON" if fan1.get_on() else "OFF"}")
+
+print("\nFAN2:")
+print(f"Speed : {fan2.get_speed()}")
+print(f"Radius : {fan2.get_radius()}")
+print(f"Color : {fan2.get_color()}")
+print(f"Status : {"ON" if fan2.get_on() else "OFF"}")
