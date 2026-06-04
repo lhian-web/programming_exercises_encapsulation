fcdb60a1c7a2c2c962b85cec779a4d259da538e2
unknown <jhullianchica@gmail.com>
2026-06-04 16:24:49 +0800

    Added text color for fan colors

--- displays_fan_properties/test_fan.py
+++ displays_fan_properties/test_fan.py
@@ -5,14 +5,24 @@ fan2 = Fan(Fan.MEDIUM, 5, "Blue", False)
 
 print("\nFAN PROPERTIES")
 
+colors = {
+    "Red": "\033[31m",
+    "Green": "\033[32m",
+    "Yellow": "\033[33m",
+    "Blue": "\033[34m",
+    "Reset": "\033[0m"
+}
+
 print("\nFAN1:")
 print(f"Speed : {fan1.get_speed()}")
 print(f"Radius : {fan1.get_radius()}")
-print(f"Color : {fan1.get_color()}")
+color1 = fan1.get_color().capitalize()
+print(f"Color : {colors.get(color1, '')}{color1}{colors['Reset']}")
 print(f"Status : {"ON" if fan1.get_on() else "OFF"}")
 
 print("\nFAN2:")
 print(f"Speed : {fan2.get_speed()}")
 print(f"Radius : {fan2.get_radius()}")
-print(f"Color : {fan2.get_color()}")
+color2 = fan2.get_color().capitalize()
+print(f"Color : {colors.get(color2, '')}{color2}{colors['Reset']}")
 print(f"Status : {"ON" if fan2.get_on() else "OFF"}")
